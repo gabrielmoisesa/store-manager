8abc54e32550bd90e83602de58b043602280cc9c
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-11-22 00:49:50 -0300

    req 1: create product mock & product model initial test

---
+++ backend/tests/unit/mocks/product.mock.js
@@ -0,0 +1,24 @@
+const productsFromDB = [
+  {
+    id: 1,
+    name: 'Example Product 1',
+  },
+  {
+    id: 2,
+    name: 'Example Product 2',
+  },
+  {
+    id: 3,
+    name: 'Example Product 3',
+  },
+];
+
+const productWithIdOne = {
+  id: 1,
+  name: 'Example Product 1',
+};
+
+module.exports = {
+  productsFromDB,
+  productWithIdOne,
+};
---
+++ backend/tests/unit/models/product.model.test.js
@@ -0,0 +1,20 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const connection = require('../../../src/models/connection');
+const { productsFromDB, productWithIdOne } = require('../mocks/product.mock');
+const productModel = require('../../../src/models/product.model');
+
+describe('Product Model', function () {
+  it('should return a product with id 1 with success', async function () {
+    sinon.stub(connection, 'execute').resolves([productsFromDB[0]]);
+
+    const product = await productModel.findById(1);
+
+    expect(product).to.be.an('object');
+    expect(product).to.be.deep.equal(productWithIdOne);
+  });
+
+  afterEach(function () {
+    sinon.restore();
+  });
+});
