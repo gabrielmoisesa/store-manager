46a5dbf87fd4ec9f14ba61236a15e6d2fad4d97e
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-11-22 03:30:34 -0300

    req 2: use sale find by id query

--- backend/src/models/sale.model.js
+++ backend/src/models/sale.model.js
@@ -7,7 +7,13 @@ const findAll = async () => dbUtils.selectByQuery(
    ORDER BY sp.sale_id ASC, sp.product_id ASC;`, 
 );
 
-const findById = async (saleId) => dbUtils.selectById('sales', saleId);
+const findById = async (saleId) => dbUtils.selectByQuery(
+  `SELECT s.date, sp.product_id, sp.quantity
+   FROM StoreManager.sales_products sp
+   JOIN StoreManager.sales s ON sp.sale_id = s.id
+   WHERE sale_id = ${saleId}
+   ORDER BY sp.sale_id ASC, sp.product_id ASC;`,
+);
 
 module.exports = {
   findAll,
